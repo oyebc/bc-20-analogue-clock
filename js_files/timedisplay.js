5cb0daece244ee63a4396782a87a4ca3d9d5fde3
oreoluwade <oreoluwade@gmail.com>
2016-10-27 14:03:17 +0100

    add bootstrap-select to allow for typing in zonenames

--- js_files/timedisplay.js
+++ js_files/timedisplay.js
@@ -3,90 +3,87 @@ document.addEventListener('DOMContentLoaded', startTimer);
 
 $(function(){
     zoneInfo = zoneInfo.sort(function(a, b) {
-        return a['zoneName'].toLowerCase() - b['zoneName'].toLowerCase();
+        return a["zoneName"].toLowerCase() > b["zoneName"].toLowerCase(); //sort the timezone data alphabetically
     });
 
     var options = '';
     zoneInfo.forEach(function(item){
         selected = '';
-        if(item['zoneName'].toLowerCase() === 'africa/lagos' ){
+        if(item["zoneName"].toLowerCase() === "africa/lagos" ){ //check the item to be selected while in lower case
              selected = 'selected ="selected"';
         }
         var zoneOffset = item.gmtOffset / 3600;
         var zoneValue = item.zoneName;
-        options += '<option value = "'+ zoneOffset + '"  "'+ selected +'" >' + 
+        options += '<option value = "'+ zoneOffset + '"  '+ selected +' >' + 
         zoneValue + ' ' + ' (GMT ' + zoneOffset + ')'  + '</option>';
     })
-
-    document.getElementById('tzSelect').innerHTML = options
+    
+    document.getElementById('tzSelect').innerHTML = options //map the data obtained to the select tag on the html doc
+    
     });
-
 
 //Set the interval between counts. 1000ms translates to 1 second
 function startTimer() {
     setInterval(displayTime, 1000);
+    displayTime(); //calling the function ensures time displays every time the page is loaded
+    audio.play() // method to play clock sound immediately page loads
 }
 
     
-//function to get current time
+//function to get current time displayed by using methods from the inbuilt Date function
 function displayTime() {
-    audio.play() // method to play clock sound immediately page loads
     var now = new Date();
-    var offset_value = document.getElementById('tzSelect').value - 1
-    var hour = now.getHours() + offset_value;
+    var offset_value = document.getElementById('tzSelect').value - 1;
+    var hour = now.getHours() + offset_value; //account for time offsets once zones are changed
     var minute = now.getMinutes();
     var second = now.getSeconds();
-    var year = now.getFullYear();
-    var month = now.getMonth() + 1;
-    var day = now.getDate();
-    
-    
-    // Digital time
-    var timeString = day + "-" + month + "-" + year + " " + addZero(formatHour(hour)) + 
-        ":" + addZero(minute) + ":" + addZero(second) + " " + 
-        getPeriod(hour);
-    document.querySelector("#current-time").innerHTML = timeString;
+    var timeString = formatHour(hour) + ":" + addZero(minute) + ":" + addZero(second) + " " + getPeriod(hour);
+    document.querySelector("#current-time").innerHTML = timeString; //Superimpose string obtained on the current-time ID
 
 
-     // --- Analog clock ---//
+
+// --- Analog clock ---//
     var canvas = document.querySelector("#clock");
     var context = canvas.getContext("2d");
-
-    //make changes to the clock size
-
+     
+    // Value of this set to less than half of the whole canvas to ensure clockhands do not exceed boundary
     var clockRadius = 180;
-
+     
     // Centers the clock in the canvas
-
-    var clockX = canvas.width / 2;
+    var clockX = canvas.width / 2;  
     var clockY = canvas.height / 2;
+     
+    Math.TAU = 2 * Math.PI; //Introduce Tau; a superior variant of PI that specifies position with refernce to the circumference
+ 
 
-    Math.TAU = 2 * Math.PI;
 
-    //Initiate drawing on the canvas created
-
+    //Write function to cater for drawing the three arms of the clock
     function drawArm(progress, armThickness, armLength, armColor) {
-        var armRadians = (Math.TAU * progress) - (Math.TAU/4);
-        var targetX = clockX + Math.cos(armRadians) * (armLength * clockRadius);
-        var targetY = clockY + Math.sin(armRadians) * (armLength * clockRadius);
-
-        context.lineWidth = armThickness;
-        context.strokeStyle = armColor;
-
-        context.beginPath(); //method to initiate revolution of hands of the clock
-        context.moveTo(clockX, clockY); // Start at the center
-        context.lineTo(targetX, targetY); // Draw a line outwards
-        context.stroke();
-    }
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    drawArm(hour / 12, 6, 0.50, '#08286D'); // Draw Hour hand, set color to navy blue
-    drawArm(minute / 60,  4, 0.8, '#08286D'); // Draw Minute hand, set color to navy blue
-    drawArm(second / 60,  1, 1.1, '#FF0000'); // Draw Second hand, set color to red
-
+    var armRadians = (Math.TAU * progress) - (Math.TAU/4);
+    var targetX = clockX + Math.cos(armRadians) * (armLength * clockRadius);
+    var targetY = clockY + Math.sin(armRadians) * (armLength * clockRadius);
+ 
+    context.lineWidth = armThickness;
+    context.strokeStyle = armColor;
+ 
+    context.beginPath();
+    context.moveTo(clockX, clockY); // Start at the center
+    context.lineTo(targetX, targetY); // Draw a line outwards
+    context.stroke();
     }
 
-    // Function will add extra zero behind, if time is less than 10 so as to always display 2 digits
 
+    /*Clears the canvas rectangle each time of extra lines
+    apart from the default clock hands to account for motion of hands*/
+    context.clearRect(0, 0, canvas.width, canvas.height); 
+
+    drawArm(hour / 12, 8, 0.50, '#000000'); // call draw arm method to draw Hour hand
+    drawArm(minute / 60,  4, 0.75, '#000000'); // call draw arm method to draw Minute hand
+    drawArm(second / 60,  2, 1.00, '#FF0000'); // call draw arm method to draw Second hand
+}
+
+
+//function created to add zero behind single digit values in the clock display
 function addZero(num) {
     if (num < 10) {
         return "0" + String(num);
@@ -96,8 +93,9 @@ function addZero(num) {
     }
 
 }
-    // Ensure the time is in the 24hr format
 
+
+    // formats 24hrs to 12hrs
 function formatHour(hour) {
     var new_hour = hour % 12;
     if (new_hour == 0) {
@@ -105,12 +103,11 @@ function formatHour(hour) {
     }
     return String(new_hour)
 }
-
 // returns AM if hour is less than 12 and PM if not
-
 function getPeriod(hour) {
     return (hour < 12) ? "AM" : "PM";
 }
+
 
     //Initiate background change if the timezone is changed from the dropdown
 
